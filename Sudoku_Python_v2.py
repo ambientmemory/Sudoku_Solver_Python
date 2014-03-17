a2da6ba38ddd9baae6658b77a394fe3a5054e84f
Piyali Mukherjee <techdruids@gmail.com>
2014-03-16 21:56:20 -0400

    This version becomes the "online" version.
    Cleaned up some syntax. Will proceed to chop more characters after
    caffeine.

---
+++ Sudoku_Python_v2.py
@@ -0,0 +1,65 @@
+import numpy as n
+def slv(ans):
+    global d,fld,mn,oc,ol
+    if d==1:return 0
+    if ocal()==0:
+        lol=ol
+        loc=oc
+        for i in range(loc):
+            ans[mn[0]][mn[1]]=lol[i]
+            fld+=1
+            if fld==81:
+                d=1
+                return 0
+            lmn=mn
+            lbd = ans
+            if slv(lbd)==-1:
+                fld-=1 
+                ans[lmn[0]][lmn[1]]=0
+                mn=lmn
+                ans = lbd
+            else:return 0
+    return -1
+def ocal():
+    global ans,oc,ol,mn
+    arc=n.full((9,9),10)
+    for i in range(9):
+        for j in range(9):
+            if ans[i][j]>0:
+                continue
+            arc[i][j]=0
+            for x in range(1,10):
+                if (not x in ans[i] and not x in ans[:,j]and sq(x,i,j)):
+                    arc[i][j]+=1
+            if arc[i][j]==0:
+                return -1
+    mn=n.unravel_index(arc.argmin(),arc.shape)
+    k=0
+    for x in range(1,10):
+        if (not x in ans[mn[0]] and not x in ans[:,mn[1]]and sq(x,mn[0],mn[1])):
+            ol[k]=x
+            k+=1
+    oc=k+1
+    return 0
+def sq(a,x,y):
+    global ans
+    for p in range(3):
+        for q in range(3):
+            if ans[(x/3)*3+p][(y/3)*3+q]==a:
+                return 0
+    return 1
+print "Yo"
+v = input()
+for i in range(v):
+    b = []
+    d=0 
+    ol=n.zeros(9)
+    oc=10
+    mn=[10,10]
+    for j in range(9):
+        b.append([int(k) for k in raw_input().split()])
+    ans = n.array(b)
+    fld=n.count_nonzero(ans)
+    print"Problem is: \n",ans    
+    if slv(ans)==-1:print"No solution found..."
+    else:print"Solution is \n",ans
